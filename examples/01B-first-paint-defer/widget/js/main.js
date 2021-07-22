d22e2fdcb6741b366f8a51081bddeeb954f5cae9
Marcin Zdun <m.zdun@samsung.com>
2021-07-22 11:47:19 +0200

    Add first-paint and complete demos with async JS

---
+++ examples/01B-first-paint-defer/widget/js/main.js
@@ -0,0 +1,48 @@
+function ii(i, len) {
+    let s = i + "";
+    while (s.length < len) s = "0" + s;
+    return s;
+}
+
+function log(msg) {
+    let now = new Date();
+    now = ii(now.getHours(), 2) + ':' + ii(now.getMinutes(), 2) + ':' + ii(now.getSeconds(), 2) + '.' + ii(now.getMilliseconds(), 3);
+
+    const time = document.createElement('span');
+    time.setAttribute('class', 'log-time');
+    time.append(now)
+
+    const line = document.createElement('span');
+    line.append(time, ' ' + msg);
+    
+    try {
+        const logger = document.querySelector('div#logger');
+        logger.prepend(line, document.createElement('br'));
+    } catch (ex) {
+        if (ex instanceof TypeError) {
+            // noop, output to console only
+        } else {
+            throw ex;
+        }
+    }
+
+    console.log(now + ' [DEMO] ' + msg);
+}
+
+
+window.addEventListener('focus', () => log('Focused'));
+window.addEventListener('blur', () => log('Focus lost'));
+window.addEventListener('load', () => log('Loaded'));
+
+window.addEventListener('load', () => {
+    // add eventListener for keydown
+    document.addEventListener('keydown', function (e) {
+        switch (e.keyCode) {
+            case 10009: //RETURN button
+                tizen.application.getCurrentApplication().exit();
+                break;
+        }
+    });
+});
+
+log('main.js');
